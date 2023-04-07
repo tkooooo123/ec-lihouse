15768fa899e272a9d80189e139ba3efcfa2ef401
tkooooo123 <tkooooo123@gmail.com>
2023-04-07 20:58:44 +0800

    新增管理者產品頁面清單

--- src/pages/admin/AdminProducts.js
+++ src/pages/admin/AdminProducts.js
@@ -1,84 +1,94 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 
 
 function AdminProducts() {
+    const [products, setProducts] = useState([]);
+    const [pagination, setPagination] = useState({})
 
     useEffect(() => {
-        
+        (async () => {
+            const productRes = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/products`)
+            console.log(productRes);
+            setProducts(productRes.data.products);
+            setPagination(productRes.data.pagination);
 
-            (async () => {
-                const productRes = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/products/all`)
-                console.log(productRes);
-            })()
+        })()
     }, [])
 
 
     return (
         <div className='p-3'>
-          <h3>產品列表</h3>
-          <hr />
-          <div className='text-end'>
-            <button type='button' className='btn btn-primary btn-sm'>
-              建立新商品
-            </button>
-          </div>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th scope='col'>分類</th>
-                <th scope='col'>名稱</th>
-                <th scope='col'>售價</th>
-                <th scope='col'>啟用狀態</th>
-                <th scope='col'>編輯</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>分類</td>
-                <td>名稱</td>
-                <td>價格</td>
-                <td>啟用</td>
-                <td>
-                  <button type='button' className='btn btn-primary btn-sm'>
-                    編輯
-                  </button>
-                  <button
-                    type='button'
-                    className='btn btn-outline-danger btn-sm ms-2'
-                  >
-                    刪除
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-    
-          <nav aria-label='Page navigation example'>
-            <ul className='pagination'>
-              <li className='page-item'>
-                <a className='page-link disabled' href='/' aria-label='Previous'>
-                  <span aria-hidden='true'>&laquo;</span>
-                </a>
-              </li>
-              {[...new Array(5)].map((_, i) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <li className='page-item' key={`${i}_page`}>
-                  <a className={`page-link ${i + 1 === 1 && 'active'}`} href='/'>
-                    {i + 1}
-                  </a>
-                </li>
-              ))}
-              <li className='page-item'>
-                <a className='page-link' href='/' aria-label='Next'>
-                  <span aria-hidden='true'>&raquo;</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+            <h3>產品列表</h3>
+            <hr />
+            <div className='text-end'>
+                <button type='button' className='btn btn-primary btn-sm'>
+                    建立新商品
+                </button>
+            </div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th scope='col'>分類</th>
+                        <th scope='col'>名稱</th>
+                        <th scope='col'>售價</th>
+                        <th scope='col'>啟用狀態</th>
+                        <th scope='col'>編輯</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => {
+                        return (
+                            <tr key={product.id}>
+                                <td>{product.category}</td>
+                                <td>
+                                    {product.title}
+                                </td>
+                                <td>{product.price}</td>
+                                <td>{product.is_enable ? '啟用' : '未啟用'}</td>
+                                <td>
+                                    <button type='button' className='btn btn-primary btn-sm'>
+                                        編輯
+                                    </button>
+                                    <button
+                                        type='button'
+                                        className='btn btn-outline-danger btn-sm ms-2'
+                                    >
+                                        刪除
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+
+                </tbody>
+            </table>
+
+            <nav aria-label='Page navigation example'>
+                <ul className='pagination'>
+                    <li className='page-item'>
+                        <a className='page-link disabled' href='/' aria-label='Previous'>
+                            <span aria-hidden='true'>&laquo;</span>
+                        </a>
+                    </li>
+                    {[...new Array(5)].map((_, i) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <li className='page-item' key={`${i}_page`}>
+                            <a className={`page-link ${i + 1 === 1 && 'active'}`} href='/'>
+                                {i + 1}
+                            </a>
+                        </li>
+                    ))}
+                    <li className='page-item'>
+                        <a className='page-link' href='/' aria-label='Next'>
+                            <span aria-hidden='true'>&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-      );
+    );
 }
 
 export default AdminProducts
