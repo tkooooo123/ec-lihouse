22976bed83c87617f51bdc1f672bb26516da0d48
tkooooo123 <tkooooo123@gmail.com>
2023-04-12 20:31:29 +0800

    新增 變更購物車商品數量功能

--- src/pages/front/Cart.js
+++ src/pages/front/Cart.js
@@ -1,20 +1,40 @@
 import axios from "axios";
+import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 
 function Cart() {
     const { cartData, getCart } = useOutletContext();
+    const[loadingItems, setLoadingItem] = useState([])
     console.log(cartData)
-    const removeCartItem = async(id) => {
+    const removeCartItem = async (id) => {
         try {
             const res = await axios.delete(`/v2/api/${process.env.REACT_APP_API_PATH}/cart/${id}`,);
-        console.log(res)
-        getCart();
+            console.log(res)
+            getCart();
         } catch (error) {
             console.log(error)
         }
-        
-
+    }
+    const updateCartItem = async (item, quantity) => {
+        try {
+            const data = {
+                data: {
+                    product_id: item.product_id,
+                    qty: quantity
+                }
+            }
+            setLoadingItem([...loadingItems, item.id])
+            const res = await axios.put(`/v2/api/${process.env.REACT_APP_API_PATH}/cart/${item.id}`,
+                data);
+            console.log(res)
+            setLoadingItem(
+                loadingItems.filter((loadingObject) => loadingObject !== item.id),
+              );
+            getCart();
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
@@ -35,21 +55,27 @@ function Cart() {
                             <div className="item-info m-3">
                                 <div className="item-title">{item.product.title}</div>
                                 <div className="item-quantity mt-3 ">
-                                    <div className="input-group border" style={{ width: '150px' }}>
-                                        <div className="input-group-prepend">
-                                            <button type="button" className="btn btn-outline-dark rounded-0 border-0"
+                                    <div className="input-group align-items-center" style={{ width: '150px' }}>
+                                       <select name="" className="form-select" id=""
+                                       value={item.qty}
+                                       disabled={loadingItems.includes(item.id)}
+                                       onChange={
+                                        (e) => {
+                                            updateCartItem(item, e.target.value*1)
+                                        }
+                                       }
+                                       >
+                                        {
+                                            [...(new Array(20))].map((i, num) => {
+                                                return(
+                                                    <option value={num + 1} key={num}>{num + 1}</option>
 
-                                            ><i className="bi bi-dash-lg"></i></button>
-                                        </div>
+                                                )
 
-                                        <input type="number" className="form-control border-0 text-center"
-                                            value={item.qty}
-                                            readOnly />
-                                        <div className="input-group-append">
-                                            <button type="button" className="btn btn-outline-dark rounded-0 border-0"
-
-                                            ><i className="bi bi-plus-lg"></i></button>
-                                        </div>
+                                            })
+                                        }
+                                        
+                                       </select>
 
                                     </div>
 
@@ -57,18 +83,18 @@ function Cart() {
                                 <div className="item-price position-absolute" style={{ right: '30px', bottom: '10px' }}>NT$ {item.total}</div>
                             </div>
                             <button className="btn remove-btn position-absolute" style={{ top: '5px', right: '5px' }}
-                            onClick={() => removeCartItem(item.id)}
+                                onClick={() => removeCartItem(item.id)}
                             >X</button>
                         </div>
                     )
 
                 })}
                 <div className="total-price d-flex aligm-items-center justify-content-between mt-3" ><h4>總金額</h4>
-                <div>NT$ {cartData.final_total}</div></div>
+                    <div>NT$ {cartData.final_total}</div></div>
                 <button type="button" className="btn btn-dark mb-2">確認</button>
 
             </div>
-            
+
 
         </div>
     )
