b910e8df74ff99fbceab9829b646c58595d886c5
tkooooo123 <tkooooo123@gmail.com>
2023-04-17 17:02:03 +0800

    新增購買心得欄位

--- src/pages/front/Home.js
+++ src/pages/front/Home.js
@@ -7,6 +7,7 @@ import SwiperBanner from "../../components/SwiperBanner";
 
 function Home() {
   const [products, setProducts] = useState([]);
+  const [feedback, setFeedback] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isCopy, setIsCopy] = useState(false);
   const [, dispatch] = useContext(MessageContext);
@@ -44,54 +45,72 @@ function Home() {
       handleErrorMessage(dispatch, error)
     }
   };
-
-  
-
+  const getFeedback = () => {
+    const dummyData = [
+      {
+        id: 1,
+        avatar: 'https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png',
+        name: '林先生',
+        product: '花漫四季-寵尚天綜合花草墊料',
+        feedback: '發貨速度快，包裝完整、乾淨，貨物齊全且完好無缺'
+      },
+      {
+        id: 2,
+        avatar: 'https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png',
+        name: '蔡小姐',
+        product: '倉鼠飛盤跑輪',
+        feedback: '家中的小倉鼠很喜歡，每天勤奮的跑'
+      },
+      {
+        id: 3,
+        avatar: 'https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png',
+        name: '李小姐',
+        product: '夢遊仙境-寵尚天綜合花草墊料',
+        feedback: '味道好聞 能吃也能當裝飾品'
+      }
+    ]
+    setFeedback([...dummyData]);
+  }
   useEffect(() => {
     getProducts();
+    getFeedback();
   }, [])
   return (
     <>
       <div className="container mt-3">
-      <SwiperBanner></SwiperBanner>
+        <SwiperBanner></SwiperBanner>
         <div className="row coupon">
           <div className="col-lg-6">
             <h2 className="fw-bold mt-5">歡慶開幕</h2>
             <hr />
             <div className="text-center text-lg-start">
-            <p className="fs-5 fw-bold">開幕優惠，即日起輸入優惠碼
-              <span className="fs-3">PET888</span>
-              享全館商品打
-              <span className="fs-3">88</span>
-              折
-            </p>
-            <button type="button" className="btn btn-primary rounded-0 p-3"
-            onClick={() => {
-              copy(copyRef.current.innerText);
-              setIsCopy(true);
-              }}>
-              <span>
-                {isCopy ? <i className="bi bi-clipboard-check me-2"></i> : <i className="bi bi-clipboard me-2"></i>}
-                {isCopy ? '複製成功！' : '複製優惠碼'}</span>
-            </button>
-
+              <p className="fs-5 fw-bold">開幕優惠，即日起輸入優惠碼
+                <span className="fs-3">PET888</span>
+                享全館商品打
+                <span className="fs-3">88</span>
+                折
+              </p>
+              <button type="button" className="btn btn-primary rounded-0 p-3"
+                onClick={() => {
+                  copy(copyRef.current.innerText);
+                  setIsCopy(true);
+                }}>
+                <span>
+                  {isCopy ? <i className="bi bi-clipboard-check me-2"></i> : <i className="bi bi-clipboard me-2"></i>}
+                  {isCopy ? '複製成功！' : '複製優惠碼'}</span>
+              </button>
             </div>
-           
           </div>
           <div className="col-lg-6 d-flex justify-content-center align-items-center mt-5">
             <div className="ticket mt-3 text-center text-white bg-primary py-3 fs-5">
               <span className="ticket-text fs-4">優惠碼</span>
               <span className="ticket-dash  mx-2" ></span>
               <span className="ticket-code fs-4" ref={copyRef}>PET8888</span>
-
             </div>
-
-
           </div>
         </div>
-
         <div className="new ">
-          <h2 className="fw-bold mt-5">最新商品
+          <h2 className="fw-bold mt-5 border-start border-primary border-5 ps-3">最新商品
             <Link to="/products">
               <span className="fs-5 mx-3">more +</span>
             </Link>
@@ -136,27 +155,47 @@ function Home() {
             })}
           </div>
         </div>
-      </div>
-      <div className="bg-light mt-7">
-        <div className="container" >
-          
+        <div className=" mt-5">
+          <h2 className="fw-bold mt-5 border-start border-primary border-5 ps-3">購買心得</h2>
+          <div className="">
+            <div className="d-flex justify-content-between row">
+              {feedback.map((content) => {
+                return (
+                  <div className="col-lg-4" key={content.id}>
+                    <div className="bg-light m-2 p-2">
+                      <h4 className="fw-bold mt-3 text-primary">{content.product}</h4>
+                      <div className="d-flex align-items-center">
+                        <img src={content.avatar} alt="avatar" style={{ width: '60px', height: '60px' }} />
+                        <div className="ms-2 mt-3">
+                          <p className="fw-bold fs-5">{content.name}</p>
+                          <p className="fw-bold fs-5 text-muted">{content.feedback}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="bg-light py-7">
+
+      <div className="bg-light py-7 mt-5">
         <div className="container">
           <img src="" alt="" />
-          <div className="row justify-content-center" style={{backgroundImage: 'url("https://images.chinatimes.com/newsphoto/2021-06-18/656/20210618001436.jpg")'}}>
+          <div className="row justify-content-center" style={{ backgroundImage: 'url("https://images.chinatimes.com/newsphoto/2021-06-18/656/20210618001436.jpg")' }}>
             <div className="col-md-4 text-center">
               <h3>Subscribe</h3>
               <p className="text-muted">訂閱我們，隨時收到最新優惠通知！</p>
               <div className="input-group mb-5">
-                <input className="form-control" type="email" placeholder="請輸入 Email..."/>
+                <input className="form-control" type="email" placeholder="請輸入 Email..." />
                 <div className="input-group-append">
                   <button className="btn btn-primary rounded-0">訂閱</button>
                 </div>
               </div>
-       
+
             </div>
           </div>
         </div>
