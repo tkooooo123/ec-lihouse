64ad0d9899eb55468e87cf65caf5e6b405ae9d6c
tkooooo123 <tkooooo123@gmail.com>
2023-05-15 00:23:16 +0800

    新增swiper圖片文字描述

--- src/components/SwiperBanner.js
+++ src/components/SwiperBanner.js
@@ -21,16 +21,16 @@ function SwiperBanner() {
 
         >
             <SwiperSlide>
-                <img src="https://cdn.hk01.com/di/media/images/dw/20220503/598221776203288576687042.jpeg/4-Da4DZToAxwkdfgnO9OG9P23Cdm5ealbUkTsG1JE7A?v=w1920r16_9" alt="..." />
+                <img src="https://cdn.hk01.com/di/media/images/dw/20220503/598221776203288576687042.jpeg/4-Da4DZToAxwkdfgnO9OG9P23Cdm5ealbUkTsG1JE7A?v=w1920r16_9" alt="全新開幕" />
                 <div className='open-text d-flex align-items-center justify-content-center'>
                     <p className='fw-bold text-primary'>全新開幕</p>
                 </div>
          
             </SwiperSlide>
             <SwiperSlide>
-                <img src="https://article.murata.com/sites/default/files/styles/large/public/static/ja-jp/images/article/iot-changes-life-with-pets/iot-changes-life-with-pets-main.jpg?itok=fXIwQlD2" alt="..." />
+                <img src="https://article.murata.com/sites/default/files/styles/large/public/static/ja-jp/images/article/iot-changes-life-with-pets/iot-changes-life-with-pets-main.jpg?itok=fXIwQlD2" alt="滿額免運" />
                 <div className='transport-text text-center text-primary'>
-                    <img src="https://www.svgrepo.com/download/22694/delivery-truck.svg" style={{ width: '50px', height: '50px' }} alt="..." />
+                    <img src="https://www.svgrepo.com/download/22694/delivery-truck.svg" style={{ width: '50px', height: '50px' }} alt="運輸icon" />
                     <h4 className='fw-bold'>滿額免運</h4>
                     <p className='fw-bold'>商品滿NT$ 699免運</p>
                 </div>
@@ -39,7 +39,7 @@ function SwiperBanner() {
                 >前往購物</Link>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="https://i.imgur.com/SV3Daha.jpeg" alt="..." style={{ objectFit: 'cover', objectPosition: 'left top' }} />
+                <img src="https://i.imgur.com/SV3Daha.jpeg" alt="新品上市" style={{ objectFit: 'cover', objectPosition: 'left top' }} />
                 <div className='new-text text-center '>
                     <p className='fw-bold text-primary mt-3'>新品上市</p>
                 </div>
